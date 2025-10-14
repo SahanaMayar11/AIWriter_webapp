867f9fb2be9c8e03606a41b9ddddf4db6f44ab74
Sahana O M <sahana.mayar08@gmail.com>
2025-10-14 16:07:29 +0000

    there is no option to hide and show the side bar

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -47,7 +47,7 @@ export default function PageHeader() {
 
   return (
     <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-      <SidebarTrigger className="sm:hidden" />
+      <SidebarTrigger />
       <h1 className="text-xl font-semibold md:text-2xl font-headline hidden sm:block">
         {capitalize(pageName.replace('-', ' '))}
       </h1>
