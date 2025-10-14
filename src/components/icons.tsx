85bf6a63a325b3f362ec03acf89cbaaf01f4063b
Sahana O M <sahana.mayar08@gmail.com>
2025-10-14 16:10:02 +0000

    Add icon of writing here instead the circle (_for element <SidebarHeader

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,12 +4,17 @@ export const Icons = {
   logo: (props: LucideProps) => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 256 256"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       {...props}
-      >
-      <rect width="256" height="256" fill="none"/>
-      <path d="M168,224l-48-96-48,96,48-24Z" opacity="0.2"/>
-      <path d="M128,24a104,104,0,1,0,104,104A104.1,104.1,0,0,0,128,24Zm40,200L120,128,72,224l56-28,40,28Z" fill="currentColor"/>
+    >
+      <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" />
     </svg>
   ),
 };
